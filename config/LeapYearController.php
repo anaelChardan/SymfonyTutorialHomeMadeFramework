48451483138ba36be5bff6c81f1c5b8335234536
Anael CHARDAN <anael.chardan@gmail.com>
2017-03-27 23:53:23 +0200

    Finish HttpKernel part

---
+++ config/LeapYearController.php
@@ -0,0 +1,28 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LeapYearController
+{
+    /**
+     * @param $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        if ($this->is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+
+    function is_leap_year($year = null) {
+        if (null === $year) {
+            $year = date('Y');
+        }
+
+        return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,7 @@
 <?php
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-
-function is_leap_year($year = null) {
-    if (null === $year) {
-        $year = date('Y');
-    }
-
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
 
 $routes->add('hello', new Routing\Route('/hello/{name}', array(
     'name' => 'World',
@@ -20,15 +11,9 @@ $routes->add('hello', new Routing\Route('/hello/{name}', array(
 )));
 
 $routes->add('bye', new Routing\Route('/bye'));
-$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-]));
+    '_controller' => 'LeapYearController::indexAction',
+)));
 
 return $routes;
